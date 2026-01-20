4c3e71b72faf7199eab6e7cdb055bd681cf58d45
GuimaraesNA <luis.guimaraes@novamerica.com.br>
2026-01-20 11:42:32 -0300

    Primeira versão do Mini App Telegram

---
+++ frontend/js/app.js
@@ -0,0 +1,8 @@
+const tg = window.Telegram.WebApp;
+
+// Expande o Mini App (fica tela cheia)
+tg.expand();
+
+// Muda a cor da barra superior do Telegram
+tg.setHeaderColor("#2aabee");
+tg.setBackgroundColor("#f5f5f5");
